002895b9c38b3cf50cdfb7aa6d4c6d9b59b1ba73
Artur Zacniewski <a.zacniewski@gmail.com>
2025-02-02 12:42:55 +0100

    Remove redundant log line

--- assets/js/custom_right_click.js
+++ assets/js/custom_right_click.js
@@ -1,5 +1,3 @@
 <!-- Prevents right-clicking on photo -->
 const noRightClick = document.getElementById("profile");
-console.log(noRightClick);
-
 noRightClick.addEventListener("contextmenu", e => e.preventDefault());
